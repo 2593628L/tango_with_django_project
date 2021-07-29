fa428a1079b1d9d6184ae7359efe20e993483fa8
ShiqiLong <2593628L@student.gla.ac.uk>
2021-07-29 09:30:58 +0800

    add register page

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Page,Category
+from django.forms import fields
+from django.contrib.auth.models import User
+from rango.models import Page,Category,UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.MAX_SIZE,help_text="Please enter the category name.")
@@ -28,4 +30,18 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
         return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username','email','password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website','picture',)
+
+
         
